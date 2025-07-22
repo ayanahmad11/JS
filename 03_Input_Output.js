fec637052060cb4ef2d3e591dcdda5421733a6d3
ayanahmad11 <ayan2011032@akgec.ac.in>
2025-07-22 07:30:08 +0530

    03

--- 03_Input_Output.js
+++ 03_Input_Output.js
@@ -1,8 +1,8 @@
-//? prompt() -  Getting input with prompt() {Browser Only }
+//? prompt() -  Getting input with prompt() [ Browser Only ]
 // creates a  pop-up window with text-field , asking the user for 
 // input . The input is always returned as a string .
 // This method is only available in browsers
-
+//* If the user cancels the prompt() returns the null value
 //? alert - displaying messages with alert()   [ Browser Only ] 
 /* 
  * alert() is a blocking pop-up message that pauses all interaction on the page untill
@@ -24,6 +24,6 @@ let rl = readline.createInterface({
 rl.question("what is your name?\n",function(name){
     console.log(`Hi, ${name}`);  
     rl.close(); 
-});
+})
 
 // outputting using console.log() works everywhere - browser and node.js
