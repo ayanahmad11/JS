3cf28b878f57618e12d687cdac9dbebc2b19034f
ayanahmad11 <ayan2011032@akgec.ac.in>
2025-07-23 08:06:36 +0530

    04

--- 07_Function.js
+++ 07_Function.js
@@ -1,28 +1,33 @@
 // 
-greet();
+// function declaration and function expression
+
+//* 1> In function declarations, the function is hoisted to the
+//*   the top of the code, meaning you can call it before 
+//*    it is defined.
+
+greet(); //?  " Hoisting "
 
 function greet(){
         console.log("Hi there");
     
 }    
-// function declaration and function expression
-
-// 1> In function declarations, the function is hoisted to the
-//    the top of the code, meaning you can call it before 
-//    it is defined.
-
-// 2> A function expression assigns a function to a variable.
-//    It not hoisted , so you can only call it after the function 
-//    is defined.
+//* 2> A function expression assigns a function to a variable.
+//*    It not hoisted , so you can only call it after the function 
+//*    is defined.
 
 let myFunc = function okay(){
         console.log("Hi there")
 }
 // okay() //  error
-
 myFunc();
 
-//? Hoisting 
+function greetUser(name ="Guest")
+{
+    console.log(`Hello,${name}`);
+}
+console.log()
+greetUser("Alice")
+greetUser();
 
 function greeting(name){
     console.log(`Hi,${name}`)
@@ -37,6 +42,6 @@ function adder(a,b = 0){ // default paramter as b = 0
 let totalSum = adder(10,20);    
 console.log(totalSum);
 
-setTimeout(function(name){
-    console.log("Hi there")
-},2000);    
+// setTimeout(function(name){
+//     console.log("Hi there")
+// },2000);    
